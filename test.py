5b3884c0e6c0cb38fcb3e0eb8e7979610203b791
kazu0716 <kazu2055.tennis@gmail.com>
2018-10-11 20:40:06 +0900

    add function of inserting data to db

--- test.py
+++ test.py
@@ -3,6 +3,17 @@ import imagehash
 import glob
 from tinydb import Query, TinyDB
 
+db = TinyDB("./db.json")
+table = db.table("image-hash", cache_size=10000)
+query = Query()
+
+
+def insert_db(data):
+    try:
+        table.insert(data)
+    except Exception as e:
+        print(e)
+
 
 def main():
     image_list = glob.glob("./images/*.png", recursive=True)
