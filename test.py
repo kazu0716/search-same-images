6796b40956ee7e42718fc48b9201fdbe5844e970
kazu0716 <kazu2055.tennis@gmail.com>
2018-10-11 21:15:56 +0900

    check if data exsit or not

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
+# -*- using:utf-8 -*-
+import time
 from PIL import Image
 import imagehash
 import glob
+import pickle
 from tinydb import Query, TinyDB
 
 db = TinyDB("./db.json")
@@ -10,16 +13,34 @@ query = Query()
 
 def insert_db(data):
     try:
-        table.insert(data)
+        result = table.search(query.file_name == data["file_name"])
+        if result == []:
+            table.insert(data)
+        else:
+            table.update(data)
     except Exception as e:
         print(e)
+
+
+def gen_hash(path):
+    return imagehash.whash(Image.open(path))
 
 
 def main():
     image_list = glob.glob("./images/*.png", recursive=True)
     for image_file in image_list:
-        print(image_file)
+        image_hash = gen_hash(image_file)
+        data = {
+            "file_name": image_file.split("/")[-1],
+            "full_path": image_file,
+            "image_hash": str(image_hash),
+            "row_hash": repr(image_hash)
+        }
+        insert_db(data)
 
 
 if __name__ == '__main__':
+    print("---program start---")
+    start = time.time()
     main()
+    print ("elapsed_time:{0}".format(time.time()-start) + "[sec]")
