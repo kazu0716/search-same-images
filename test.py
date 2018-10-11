52eaa156ed67c5892a6ba02196b16b7cdfd9f2f6
kazu0716 <kazu2055.tennis@gmail.com>
2018-10-11 20:39:07 +0900

    show filename in dir

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 from PIL import Image
 import imagehash
 import glob
+from tinydb import Query, TinyDB
+
+
+def main():
+    image_list = glob.glob("./images/*.png", recursive=True)
+    for image_file in image_list:
+        print(image_file)
+
 
 if __name__ == '__main__':
-    print(glob.glob('images/*.png', recursive=True))
+    main()
